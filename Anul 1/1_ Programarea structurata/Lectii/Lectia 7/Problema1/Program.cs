c3e9c8ca8b6a62f84a8d12663f83cacddcaf306b
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:53:22 +0300

    Lectia 7

---
+++ Anul 1/1_ Programarea structurata/Lectii/Lectia 7/Problema1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            char Operator;
+            Console.Write("Operator : ");
+            Operator = char.Parse(Console.ReadLine());
+
+            switch (Operator)
+            {
+                case '+': Console.WriteLine($"{a} + {b} = {a+b}"); break;
+                case '-': Console.WriteLine($"{a} - {b} = {a-b}"); break;
+                case '*': Console.WriteLine($"{a} * {b} = {a*b}"); break;
+                case '/': Console.WriteLine($"{a} / {b} = {a/b}"); break;
+                case '%': Console.WriteLine($"{a} % {b} = {a%b}"); break;
+                default:
+                    Console.WriteLine("Operator necunoscut\n0");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
