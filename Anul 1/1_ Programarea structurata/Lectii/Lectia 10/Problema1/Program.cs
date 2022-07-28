f98f2b5fd75d51a2aa9a3132ea23101cc3dea9ee
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:53:42 +0300

    Lectia 10

---
+++ Anul 1/1_ Programarea structurata/Lectii/Lectia 10/Problema1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Problema 1:
+             Sa se determine cel mai mare numar care se poate forma 
+             cu ajutorul cifrelor unui numar natural citit de la tastatura.
+             */
+            Console.WriteLine("Introduceti un numar: ");
+            string nr = Console.ReadLine();
+            int[] digits = new int[nr.Length];
+            for (int i = 0; i < nr.Length; i++)
+            {
+                digits[i] = int.Parse($"{nr[i]}");
+            }
+            Array.Sort(digits);
+            Array.Reverse(digits);
+            string formated = "";
+            for (int i = 0; i < nr.Length; i++)
+            {
+                formated += digits[i];
+            }
+            Console.WriteLine($"Numarul maxim format: {formated}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Lectii/Lectia 10/Problema2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Problema 2.
+            Se citește numărul natural n, şi două caractere c şi d.
+            Să se afișeze următorul pătrat, format din n linii şi n coloane:
+
+             ccc...cc
+             cdd...dc
+             ....
+             cdd...dc
+             ccc...cc
+            */
+
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti caracterul 1 : ");
+            char ch1 = char.Parse(Console.ReadLine());
+            Console.Write("Introduceti caracterul 2 : ");
+            char ch2 = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{ch1} ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write($"{ch1} ");
+                for (int f = 0; f < n - 2; f++)
+                {
+                    Console.Write($"{ch2} ");
+                }
+                Console.WriteLine($"{ch1} ");
+            }
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{ch1} ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
