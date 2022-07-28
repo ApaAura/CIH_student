0758b5431c9c02199ac0ce3937b6feef9c7ebaa8
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:47:51 +0300

    Laboratorul 3

---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 3/Problema3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Citirea datelor initiale
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y=");
+            double y = double.Parse(Console.ReadLine());
+
+            //Blocul de calcul 
+            double f = ((Math.Abs(x - y) / (x + Math.Sin((Math.PI) / (y) * (Math.Cos((Math.PI) / x))))));
+
+            //Afisarea datelor 
+            Console.WriteLine($"f({x}, {y}) = {f:F4}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 3/Problema4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problema4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Citirea datelor initiale
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y=");
+            double y = double.Parse(Console.ReadLine());
+
+            //Blocul de calcul 
+            double f = Math.Sqrt(Math.Abs(x - y) / Math.Sqrt(1 / Math.Abs(x - y)) * (Math.Abs(x) + 1));
+
+            //Afisarea datelor 
+            Console.WriteLine($"f({x}, {y}) = {f:F4}");
+
+            Console.ReadKey();
+        }
+    }
+}
