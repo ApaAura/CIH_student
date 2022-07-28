8de47f0355a89a9841bf47f11b06c5ad600a8bb7
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:49:17 +0300

    Laboratorul 11

---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 11/App1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        /*
+        Se dă un șir cu n elemente, numere naturale. Determinați diferența în valoare absolută dintre
+        numărul de valori pare din șir și numărul de valori impare din șir.
+        */
+        static void Main(string[] args)
+        {
+
+            //Citirea datelor 
+            int n = int.Parse(Console.ReadLine());
+            string linie = Console.ReadLine();
+            string[] element = linie.Split(' ');
+            int[] tabel = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                tabel[i] = int.Parse(element[i]);
+            }
+            /* 
+            Citirea datelor aleatoriu 
+            Random el = new Random ()
+            for(int i=0; i<n; i++)
+               {
+                  tabel[i] = el.Next(100);
+               }
+            */
+            int elementePare = 0, elementeImpare = 0;
+            for (int i = 0; i < tabel.Length; i++)
+            {
+                if (tabel[i] % 2 == 0)
+                {
+                    elementePare++;
+                }
+                else
+                {
+                    elementeImpare++;
+                }
+            }
+        
+            int c = Math.Abs(elementePare - elementeImpare);
+            /*
+            Console.Write.Line(c);
+            Console.ReadKey();
+            */
+
+            //Afisare rezultat
+            Console.WriteLine("Tabelul Initial: ");
+            for(int i=0; i<tabel.Length; i++) 
+            {
+                Console.Write("{0} ", tabel[i]);
+            }
+            Console.WriteLine($"\n|{elementePare}-{elementeImpare}| = {c}");
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 11/App2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Citirea datelor initiale
+            int n = int.Parse(Console.ReadLine());
+            int[] tabel = new int[n];
+
+            string linie = Console.ReadLine();
+            string[] element = linie.Split(' ');
+            for (int i=0; i < n; i++)
+            {
+                tabel[i] = int.Parse(element[i]);
+            }
+            /*
+            Citirea datelor aleatoriu 
+            Random el = new Random ()
+            for(int i-=0; i<n; i++)
+               {
+                  tabel[i] = el.Next(100);
+               }
+            */
+
+            //Algoritmul de prelucrare a tabelului 
+            int min = tabel[0];
+            int max = tabel[0];
+            for (int i = 1; i < tabel.Length; i++)
+            {
+                if (min > tabel[i])
+                {
+                    min = tabel[i];
+                }
+                if (max < tabel[i])
+                {
+                    max = tabel[i];
+                }
+            }
+            //Console.WriteLine("{0} {1}", min, max);
+            //Afisare rezultat
+            Console.WriteLine("Tabelul Initial: ");
+            for (int i = 0; i < tabel.Length; i++)
+            {
+                Console.Write("{0} ", tabel[i]);
+            }
+            Console.WriteLine("\nminim = {0}\nmaxim = {1}", min, max);
+        }
+    }
+}
