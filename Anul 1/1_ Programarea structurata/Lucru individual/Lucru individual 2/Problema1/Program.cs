09cc00effd6b719a29bbcaf1024a1c7ad97fad76
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:55:22 +0300

    Lucru individual nr2

---
+++ Anul 1/1_ Programarea structurata/Lucru individual/Lucru individual 2/Problema1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Problema 1: 
+             Se da numărul natural n. 
+             Să se afle media aritmetică a unui șir de n numere date.
+             */
+
+            int[] a;
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            a = new int[n];
+            int sum = 0;
+            Console.WriteLine($"Introduceti {n} numere: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Numarul {i + 1}:");
+                a[i] = int.Parse(Console.ReadLine());
+                sum += a[i];
+            }
+            Console.WriteLine($"Med: {sum/n:F2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Lucru individual/Lucru individual 2/Problema4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Problema4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Problema 4. Să se scrie un program care citește de la tastatură 
+             un șir de numere naturale și determină media aritmetică a celor pare.
+             */
+            Console.Write("Dati numarul de elmente:");
+            int elemente = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Introdu {elemente} numere intregi");
+            int[] mas = new int[elemente];
+            int pare = 0;
+            int suma = 0;
+            for (int i = 0; i < elemente; i++)
+            {
+                Console.Write($"Numarul {i + 1}:");
+                mas[i] = int.Parse(Console.ReadLine());
+                if (mas[i]%2==0)
+                {
+                    pare++;
+                    suma += mas[i];
+                }
+            }
+            if (pare == 0)
+            {
+                Console.WriteLine("nu exista");
+            }
+            else
+            {
+                Console.WriteLine($"Media aritmetica a numerelor este: {suma/pare:F2}");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
