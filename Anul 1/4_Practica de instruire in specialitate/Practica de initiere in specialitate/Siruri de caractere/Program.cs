c0b356275352e4ab8598d8c3b5a222605cb27a87
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 22:13:13 +0300

    Tema 5

---
+++ Anul 1/4_Practica de instruire in specialitate/Practica de initiere in specialitate/Siruri de caractere/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Siruri_de_caractere
+{
+    class Program
+    {
+        static public string text;
+        static public string invers;
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Introduceti un sir de caractere: ");
+             text = Console.ReadLine();
+             invers = Inversare(text, text.Length - 1);
+             Verificare(text);
+        }
+        private static void Verificare(string text)
+        {
+            if (text!=invers)
+            {
+                Console.WriteLine($"Sirul '{text}' nu este palidrom.");
+            }
+            else
+            {
+                Console.WriteLine($"Sirul '{text}' este palidrom.");
+            }
+        }
+        static public string Inversare(string sir, int length)
+        {
+            if (length == -1) return "";
+            else
+            {
+                return sir[length] + Inversare(sir, length - 1);
+            }
+        }
+    }
+}
