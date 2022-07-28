d65b1aeebc5c69262ec6a2ef041ab20cf023cf60
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 19:28:11 +0300

    Help

---
+++ Anul 1/1. Programarea structurata/Help/Prob 6/Prob 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Prob_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //De la tastatură este citit un număr natural n. Să se calculeze şi să se afişeze suma numerelor impare pînă la acest n dat.
+            Console.WriteLine("Introduceti un numar: ");
+            int n = int.Parse(Console.ReadLine());
+            int Suma = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                //verificam daca numarul este impar
+                if (i%2==1)
+                {
+                    Suma += i;
+                }
+            }
+            Console.WriteLine("Suma numerelor impare pana la {0} este {1}",n, Suma);
+        }
+    }
+}
