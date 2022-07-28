f4b6cf65b6e118d28c2bbc96a94dd378d4b042a3
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:48:30 +0300

    Laboratorul 7

---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 7/Problema2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Realizați o aplicație de consolă.
+             De la tastatură se citește un număr pozitiv, mai mic sau egal cu 100.
+             Elaborați un program care va afișa numărul citit cu cifre romane.
+            */
+
+            int a;
+            Console.Write("Introduceti un numar : ");
+            a = int.Parse(Console.ReadLine());
+            
+            
+            switch (a)
+            {
+                case 1: Console.Write("I"); break;
+                case 2: Console.Write("II"); break;
+                case 3: Console.Write("III"); break;
+                case 4: Console.Write("IV"); break;
+                case 5: Console.Write("V"); break;
+                case 6: Console.Write("VI"); break;
+                case 7: Console.Write("VII"); break;
+                case 8: Console.Write("VIII"); break;
+                case 9: Console.Write("IX"); break;
+                case 10: Console.Write("X"); break;
+
+
+                default: Console.Write("Numarul nu poate fi citit "); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 7/Problema3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Este dat un plan de coordonate și un punct (x,y) citit de la tastatură. 
+             Determinați dacă acest punct se află în domeniul hașurat din planul de coordonate.
+              */
+
+            int x,y;
+            Console.Write("x= ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > 40)
+                {
+                    if (y > 40)
+                    {
+                        Console.WriteLine("Punctul apartine domeniului hasurat");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Punctul nu apartine domeniului hasurat");
+                    }
+                }
+            if (x < -40)
+            {
+                if (y < -40)
+                {
+                    Console.WriteLine("Punctul nu apartine domeniului hasurat");
+                }
+                else
+                {
+                    Console.WriteLine("Punctul apartine domeniului hasurat");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
