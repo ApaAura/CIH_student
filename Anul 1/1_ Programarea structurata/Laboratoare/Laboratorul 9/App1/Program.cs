31a5a6c20d322941dc70ed0641fc04fa31bb7881
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:49:09 +0300

    Laboratorul 9

---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 9/App1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu numere intregi");
+            Console.WriteLine("For method");
+            int pare = 0;
+            int nr = int.Parse(Console.ReadLine());
+            for (; nr!=0 ;)
+            {
+                if (nr%2==0)
+                {
+                    pare++;
+                }
+                nr = int.Parse(Console.ReadLine());
+            }
+            if (pare == 0)
+            {
+                Console.WriteLine("nu exista");
+            }
+            else
+            {
+                Console.WriteLine($"Numere pare {pare}");
+            }
+            Console.WriteLine("while method");
+            pare = 0;
+            nr = 1;
+            while(nr != 0)
+            {
+                if (nr % 2 == 0)
+                {
+                    pare++;
+                }
+                nr = int.Parse(Console.ReadLine());
+            }
+            if (pare == 0)
+            {
+                Console.WriteLine("nu exista");
+            }
+            else
+            {
+                Console.WriteLine($"Numere pare {pare}");
+            }
+            Console.WriteLine("Do while method");
+            pare = 0;
+            nr = 1;
+            do
+            {
+                nr = int.Parse(Console.ReadLine());
+                if (nr ==0)
+                {
+                    break;
+                }
+                if (nr%2==0)
+                {
+                    pare++;
+                }
+            } while (nr!=0);
+            if (pare == 0)
+            {
+                Console.WriteLine("nu exista");
+            }
+            else
+            {
+                Console.WriteLine($"Numere pare {pare}");
+            }
+        }
+    }
+}
---
+++ Anul 1/1_ Programarea structurata/Laboratoare/Laboratorul 9/App2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            int pow = 1;
+            //For method
+            for (int i = 0; i < b; i++)
+            {
+                pow *= a;
+            }
+            Console.WriteLine($"For method: {a}^{b} = {pow}");
+            //While method
+            pow = 1;
+            int state = 0;
+            while (state<b)
+            {
+                pow *= a;
+                state++;
+            }
+            Console.WriteLine($"While method: {a}^{b} = {pow}");
+            //Do while method
+            state = 0;
+            pow = 1;
+            do
+            {
+                pow *= a;
+                state++;
+            }
+            while (state < b);
+            Console.WriteLine($"Do while method: {a}^{b} = {pow}");
+            Console.ReadKey();
+        }
+    }
+}
