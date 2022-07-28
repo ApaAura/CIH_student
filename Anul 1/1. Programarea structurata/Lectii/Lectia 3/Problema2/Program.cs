0b68678dabf2c28fa8f359ce90578aedfe5dd21a
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 19:26:40 +0300

    Lectii

---
+++ Anul 1/1. Programarea structurata/Lectii/Lectia 3/Problema2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            /* De la tastatură se citesc două numere întregi a şi b. Elaboraţi un program prin
+           intermediul căruia să se schimbe între ele valorile variabilelor a şi b. La ecran 
+           se vor afişa noile valori ale lui a şi b. Rezolvați problema fără utilizarea
+           variabilei auxiliare. */
+
+
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"Noile valori sunt a={a} b={b}");
+
+            Console.ReadKey();
+        }
+    }
+}
