0b3aefc0eaa4207d4e32a843c5ca3c99e1279608
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 22:12:15 +0300

    Tema 3

---
+++ Anul 1/4_Practica de instruire in specialitate/Practica de initiere in specialitate/ClassLibrary/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        static int[,] masiv = new int[4, 4];
+        static public void Citire()
+        {
+            Random el = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                  masiv[i,j] = el.Next(-100, 100);
+                }
+            }
+        }
+        static public void Afisare()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(masiv[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static public int[] elemente;
+        static public void Pare()
+        {
+            string s = "";
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (masiv[i,j]%2==0)
+                    {
+                        s += $"{masiv[i,j]} ";
+                    }
+                }
+            }
+            Console.WriteLine("\nNumere pare:\n"+s);
+            elemente = s.Trim().Split(" ").Select(x => int.Parse(x.Trim())).ToArray();
+        }
+        static public int Suma()
+        {
+            return elemente.Sum();
+        }
+    }
+}
---
+++ Anul 1/4_Practica de instruire in specialitate/Practica de initiere in specialitate/Metode template/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+
+namespace Metode_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Citire();
+            Class1.Afisare();
+            Class1.Pare();
+            Console.WriteLine($"\nSuma elementelor pare: {Class1.Suma()}"); 
+            Console.ReadKey();
+        }
+    }
+}
