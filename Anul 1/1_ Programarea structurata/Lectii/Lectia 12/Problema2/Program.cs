07e546dc48efd04f3023ada9c5a8add7c36b7b53
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:53:51 +0300

    Lectia 12

---
+++ Anul 1/1_ Programarea structurata/Lectii/Lectia 12/Problema2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 } };
+            //int[,] a = new int[3, 4] { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 3, 3, 3, 3 } };
+            int i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4; j++)
+                    Console.Write("{0} ", a[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
