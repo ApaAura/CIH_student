a9bf7ceba301fd728fb35a249ef70ce55d6916d7
Apareci Aura <aurica.apareci@gmail.com>
2022-07-28 21:55:03 +0300

    Lucru individual nr1

---
+++ Anul 1/1_ Programarea structurata/Lucru individual/Lucru individual 1/Problema6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problema6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            De la tastatură se întroduc lungimile laturilor unui triunghi: a,b,c.
+            Să se determine dacă ele formează triunghi echilateral
+            */
+           
+            int a, b, c;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                if (b == c)
+                {
+                    if (c == a)
+                    { Console.WriteLine("Laturile formeaza triunghi echilateral"); }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Laturile nu formeaza triunghi echilateral");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
