f679bf88e2f1744e036618b80c15903c1a891f44
Apareci Aura <aurica.apareci@gmail.com>
2022-07-31 22:04:35 +0300

    Lectia 8

---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 8/Problema 1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace Problema_1
+{
+    
+    class Program
+    {
+        const int nmax = 20;
+        public static int[] b = new int[nmax];
+        public static int n, k;
+        static void Main(string[] args)
+        {
+            using (StreamWriter wr = new StreamWriter("date.out"))
+            {
+                wr.WriteLine("Solutiile obtinute: ");
+            }
+                int r;
+                Console.Write("Introduceti n: ");
+                n = int.Parse(Console.ReadLine());
+                Console.Write("Introduceti k: ");
+                k = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    b[i] = 0;
+                }
+                do
+                {
+                    r = Numar();
+                    if (r>=k)
+                    {
+                        Scrie();
+                    }
+                    if (r<n)
+                    {
+                        Urmator(ref b);
+                    }
+                } while (r!=n);
+                 Console.ReadKey();
+        }
+        private static void Urmator(ref int[] x)
+        {
+            int j = n;
+            while (x[j]==1)
+            {
+                x[j] = 0;
+                j--;
+            }
+            x[j] = 1;
+        }
+        private static void Scrie()
+        {
+            using (StreamWriter wr=new StreamWriter("date.out", append:true))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    wr.Write($"{b[i]}, ");
+                }
+                wr.WriteLine();
+            }
+        }
+        private static int Numar()
+        {
+            int s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                s += b[i];
+            }
+            return s;
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 8/Problema 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Problema_2
+{
+    class Program
+    {
+        /*
+         Se consideră numerele naturale din mulţimea {1, 2, 3, ..., n}. Să se
+         determine toate elementele acestei mulţimi, pentru care suma
+         cifrelor este egală cu un număr dat m.
+         */
+        static void Main(string[] args)
+        {
+            int n;
+            int valoare;
+            Console.Write("Introduceti n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti valoarea: ");
+            valoare = int.Parse(Console.ReadLine());
+            int cantitate = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Solutie(i,valoare))
+                {
+                    Prelucrare(i, ref cantitate);
+                }
+            }
+            Console.WriteLine($"\n Astfel de numere sunt: {cantitate}");
+            Console.ReadKey();
+        }
+        private static void Prelucrare(int i, ref int cantitate)
+        {
+            Console.WriteLine($"Numarul={i}");
+            cantitate++;
+        }
+        private static bool Solutie(int i, int valoare)
+        {
+            if (Suma(i)==valoare)
+            {
+                return true;
+            }
+            return false;
+        }
+        private static int Suma(int i)
+        {
+            int s = 0;
+            while (i != 0)
+            {
+                s = s + (i % 10);
+                i /= 10;
+            }
+            return s;
+        }
+    }
+}
