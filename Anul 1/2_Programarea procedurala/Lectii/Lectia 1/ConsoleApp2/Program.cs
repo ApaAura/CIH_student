f8b1fd3caf52e326149db847fe45342ff1841589
Apareci Aura <aurica.apareci@gmail.com>
2022-07-31 22:03:35 +0300

    Lectia 1

---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 1/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             De creat un fisier cu denumirea orase.txt ce va contine denumirile a cinci orase,
+              ce vor fi citite de la tastatura. De afisat la ecran continutul acestu fisier
+            */
+            string oras;
+            StreamWriter fisierOrase = new StreamWriter("orase.txt");
+            Console.WriteLine("Introdu denumirile a 5 orase:");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Denumumirea orasului {i}: ");
+                oras = Console.ReadLine();
+                fisierOrase.WriteLine(oras);
+            }
+            fisierOrase.Close();
+            /*StreamReader fOrase = new StreamReader("Orase.txt");
+            while ((oras=fOrase.ReadLine())!=null)
+            {
+                Console.WriteLine(oras);
+            }
+            fOrase.Close();
+            */
+            using (StreamReader fo = new StreamReader("Orase.txt"))
+            {
+                Console.WriteLine(fo.ReadToEnd());
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 1/Problema2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creati un fisier numbers.txt si scrieti numerele naturale de la 1 la 50 separate prin virgula 
+            StreamWriter fisiernumbers = new StreamWriter("numbers.txt");
+            Console.WriteLine("Numerele de la 1 la 50: ");
+            fisiernumbers.WriteLine("Numerele de la 1 la 50: ");
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.Write($"{i}, ");
+                fisiernumbers.Write($"{i}, ");
+            }
+            fisiernumbers.Close();
+
+        }
+    }
+}
