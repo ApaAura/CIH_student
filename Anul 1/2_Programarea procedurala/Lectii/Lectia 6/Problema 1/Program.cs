b25b2f34824b6c9389cc5bf284469d2e5fe76754
Apareci Aura <aurica.apareci@gmail.com>
2022-07-31 22:04:22 +0300

    Lectia 6

---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 6/Problema 1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problema_1
+{
+    class Program
+    {
+        /*
+         De creat o stivă ce va conține informații despre lucrătorii unei companii: Numele lucrătorului, și salariul lui.
+         Să se calculeze salariul mediu în companie și să se afișeze lista lucrătorilor ce au salariul mai mic ca cel mediu
+         */
+        struct Lucrator
+        {
+            string Nume;
+            public int Salariu;
+            public void Citire()
+            {
+                Console.Write("Nume client: ");
+                Nume = Console.ReadLine();
+                Console.Write("Suma: ");
+                Salariu = int.Parse(Console.ReadLine());
+            }
+            public void Afisare()
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine($"Nume: {Nume}");
+                Console.WriteLine($"Suma: {Salariu}");
+                Console.WriteLine("===========================");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack<Lucrator> lucrator = new Stack<Lucrator>();
+            Console.Write("Introduceti numarul de lucratori: ");
+            int nr = int.Parse(Console.ReadLine());
+            Lucrator nr1;
+            for (int i = 0; i < nr; i++)
+            {
+                nr1 = new Lucrator();
+                nr1.Citire();
+                lucrator.Push(nr1);
+            }
+            // float SalariuMed =(float) lucrator.Average(x=>x.Salariu);
+            double SumaSalariu = 0;
+            foreach (Lucrator item in lucrator)
+            {
+                SumaSalariu += item.Salariu;
+            }
+            double SalariuMed = SumaSalariu / nr;
+            List<Lucrator> salariuBun = new List<Lucrator>();
+            foreach (var item in salariuBun)
+            {
+                if (item.Salariu < SalariuMed)
+                {
+                    salariuBun.Add(item);
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"Salariu mediu: {SalariuMed}");
+            Console.WriteLine("Lista lucratorilor cu salariu mai mic decat cel mediu: ");
+            foreach (Lucrator item in salariuBun)
+            {
+                item.Afisare();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 6/Problema 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_2
+{
+    class Program
+    {
+        /*
+         Să se creeze o listă legată cu elemente reale. Să se elimine din această 
+         listă elementele negative. Să se afișeze această listă modificată.
+         */
+        static void Main(string[] args)
+        {
+            List<double> numere = new List<double>();
+            Console.Write("Introduceti numarul de elemente: ");
+            int nr = int.Parse(Console.ReadLine());
+            Random el = new Random();
+            for (int i = 0; i < nr; i++)
+            {
+                double element = el.Next(-100,100);
+                numere.Add(element);
+            }
+            Console.Clear();
+            Console.WriteLine("Elementel listelei: ");
+            foreach (double item in numere)
+            {
+                Console.Write(item+" ");
+            }
+            List<double> elNegative = new List<double>();
+            Console.WriteLine("\nElementele negative ale listei: ");
+            foreach (double item in numere)
+            {
+                if (item<0)
+                {
+                    elNegative.Add(item);
+                }
+            }
+            foreach (double item in elNegative)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
