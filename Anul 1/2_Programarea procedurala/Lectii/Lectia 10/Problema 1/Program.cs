4e347588d9482797f431ea06bf081e202c7dbd5b
Apareci Aura <aurica.apareci@gmail.com>
2022-07-31 22:04:50 +0300

    Lectia 10

---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 10/Problema 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Problema_1
+{
+    class Program
+    {
+        static int n, k;
+        static int[] st = new int[20];
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("k= ");
+            k = int.Parse(Console.ReadLine());
+            st[0] = 0;
+            Btk(1);
+            Console.ReadKey();
+        }
+        private static void Btk(int p)
+        {
+            for (int val = st[p-1]+1; val <= n; val++)
+            {
+                st[p] = val;
+                if (Solutie(p))
+                {
+                    Tipar(p);
+                }
+                else
+                    Btk(p + 1);
+            }
+        }
+        private static void Tipar(int p)
+        {
+            for (int i = 0; i <= p; i++)
+            {
+                Console.Write($"{st[i]} ");
+            }
+            Console.WriteLine();
+        }
+        //Trebuie de adaugat functia Solutie 
+        private static bool Solutie(int p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 10/Problema 2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Problema_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string linie;
+            string[] el;
+            Console.WriteLine("Introdu elementele multimii pe o linie separate prin spatiu: ");
+            linie = Console.ReadLine();
+            el = linie.Split(' ');
+            n = el.Length;
+            int[] ma = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                ma[i] = int.Parse(el[i]);
+            }
+            Console.WriteLine("\nMultimea initiala: ");
+            Afisare(ma);
+            Bactracking(ma);
+            Console.ReadKey();
+        }
+        private static void Bactracking(int[] ma)
+        {
+            int k = 0;
+            int[] x = new int[ma.Length];
+            int nrSol = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = 0;
+            }
+            while (k>=0)
+            {
+                if (k==x.Length)
+                {
+                    ReturnSolutia(ma, x, ++nrSol);
+                    k--;
+                }
+                else
+                {
+                    if (x[k]<x.Length)
+                    {
+                        x[k]++;
+                        if (Continuare(x,k))
+                        {
+                            k++;
+                        }
+                    }
+                    else
+                    {
+                        x[k--] = 0;
+                    }
+                }
+            }
+        }
+        private static bool Continuare(int[] x, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                if (x[k]==x[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static void ReturnSolutia(int[] ma, int[] x, int v)
+        {
+            Console.Write($"Permutarea {v}: ");
+            for (int i = 0; i < ma.Length; i++)
+            {
+                Console.Write($"{ma[x[i]-1]} ");
+            }
+            Console.WriteLine("\n------------------------------------");
+        }
+        private static void Afisare(int[] ma)
+        {
+            foreach (var item in ma)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
