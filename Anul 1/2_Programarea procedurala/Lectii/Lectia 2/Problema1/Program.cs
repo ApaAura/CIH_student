07692c37497b1f6a94c25191615239a176dde223
Apareci Aura <aurica.apareci@gmail.com>
2022-07-31 22:03:53 +0300

    Lectia 2

---
+++ Anul 1/2_Programarea procedurala/Lectii/Lectia 2/Problema1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Problema1
+{
+    class Program
+    {
+        public static string fileName = "Nature.in";
+        static void Main(string[] args)
+        {
+            int[] tabelNote = new int[]
+            {
+                5,
+                7,
+                8,
+                9,
+                10,
+            };
+            using (BinaryWriter fout = new BinaryWriter(File.Open(fileName, FileMode.OpenOrCreate)))
+            {
+                foreach (int note in tabelNote)
+                {
+                    fout.Write(note);
+                }
+            }
+            Console.WriteLine("Valorile din fisierul binar au fost scrise");
+            using (BinaryReader reader=new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                Console.WriteLine("Valorile din fisierul dat:");
+                while (reader.PeekChar()>-1)
+                {
+                    int nota = reader.ReadInt32();
+                    Console.Write($"{nota}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
