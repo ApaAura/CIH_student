9342feec760d873d03f70cb0d676cb27e57e787f
Apareci Aura <aurica.apareci@gmail.com>
2022-08-01 23:32:45 +0300

    Laboratorul 1

---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 1/Problema1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fisierul date.in contine 3 numere intregi separate printr-un singur spatiu. 
+            Sa se elaboreze un program care va afisa aceste numere unul sib altul, 
+            afisand in dreptul fiecaruia anul dintre cuvintele PAR sau IMPAR.
+            Rezultatul va fi afisat la ecarn cat si in fisieul date.out
+             */
+            StreamReader fNumere = new StreamReader("date.in");
+            string[] stNumere = fNumere.ReadLine().Split(' ');
+            fNumere.Close();
+            int num1, num2, num3;
+            num1 = int.Parse(stNumere[0]);
+            num2 = int.Parse(stNumere[1]);
+            num3 = int.Parse(stNumere[2]);
+            using (StreamWriter fRezultat = new StreamWriter("date.out"))
+            {
+                if (num1 % 2 == 1)
+                {
+                    fRezultat.WriteLine($"{num1} - IMPAR");
+                    Console.WriteLine($"{num1} - IMPAR");
+                }
+                else
+                {
+                    fRezultat.WriteLine($"{num1} - PAR");
+                    Console.WriteLine($"{num1} - PAR");
+                }
+                if (num2 % 2 == 1)
+                {
+                    fRezultat.WriteLine($"{num2} - IMPAR");
+                    Console.WriteLine($"{num2} - IMPAR");
+                }
+                else
+                {
+                    fRezultat.WriteLine($"{num2} - PAR");
+                    Console.WriteLine($"{num2} - PAR");
+                }
+                if (num3 % 2 == 1)
+                {
+                    fRezultat.WriteLine($"{num3} - IMPAR");
+                    Console.WriteLine($"{num3} - IMPAR");
+                }
+                else
+                {
+                    fRezultat.WriteLine($"{num3} - PAR");
+                    Console.WriteLine($"{num3} - PAR");
+                }
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 1/Problema2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fisierul date.in contine 12 numere intregi separate printr-un singur spatiu 
+            care reprezinta temperaturile medii ale lunilor unui an, ca numere intregi.
+            Sa se afiseze cu 2 zecimale media anuala a temperaturilor pozitive 
+            si a rezultatelor negative.
+            Rezultatul va fi afisar la ecran cat si in fisierul date.out
+             */
+            StreamReader fNumere = new StreamReader("date.in");
+            string[] stNumere = fNumere.ReadLine().Split(" ");
+            fNumere.Close();
+            int[] temperatura = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                temperatura[i] = int.Parse(stNumere[i]);
+            }
+            int spozitive = 0, snegative = 0, npoz = 0, nneg = 0;
+            double mpozitive, mnegative;
+            for (int i = 0; i < 12; i++)
+            {
+                if (temperatura[i] > 0)
+                {
+                    npoz++;
+                    spozitive += temperatura[i];
+                }
+                else
+                {
+                    nneg++;
+                    spozitive += temperatura[i];
+                }
+            }
+            mpozitive = (double)spozitive / npoz;
+            mnegative = (double)snegative / nneg;
+            Console.WriteLine($"Media negativa: {mnegative:F2}");
+            Console.WriteLine($"Media pozitiva: {mpozitive:F2}");
+            using(StreamWriter fRezultat= new StreamWriter("date.out"))
+            {
+                    fRezultat.WriteLine($"Media negativa: {mnegative:F2}");
+                    fRezultat.WriteLine($"Media pozitive: {mpozitive:F2}");
+            }
+            Console.ReadKey();
+            
+
+        }
+    }
+}
