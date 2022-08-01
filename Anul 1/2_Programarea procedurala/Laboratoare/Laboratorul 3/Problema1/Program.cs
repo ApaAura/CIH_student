4383117ada1b0dcaf51b626f0ee02f34e716ca24
Apareci Aura <aurica.apareci@gmail.com>
2022-08-01 23:33:01 +0300

    Laboratorul 3

---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 3/Problema1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            4.Fişierul cuvinte.in conţine mai multe cuvinte, separate printr - un singur spaţiu. Elaboraţi
+            un program prin intermediul căruia la ecran se vor afişa cuvintele aranjate
+            descrescător / crescător.
+            */
+            StreamReader fin = new StreamReader("cuvinte.in");
+            string[] str = fin.ReadLine().Split(' ');
+            bool gasit;
+            do
+            {
+                gasit = false;
+                for (int i = 0; i < str.Length-1; i++)
+                {
+                    if (str[i].Length > str[i + 1].Length)
+                    {
+                        string temp = str[i + 1];
+                        str[i + 1] = str[i];
+                        str[i] = temp;
+                        gasit = true;
+                    }
+                }
+            } while (gasit);
+            fin.Close();
+            Console.WriteLine("Cuvinte din fisier aranjate in Ordine crescatoare: ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 3/Problema2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fisierul vector.in contine n numere intregi separate printr-un singur spatiu. primul numar
+            reprezinta numarul de numere din fisier. Elaborati un program prin intermediun caruia
+            primele k numere vor fi sortate descrescator, iar restul numerele crescator, k se citeste de 
+            la tastatura
+             */
+            using (StreamReader fin = new StreamReader("vector.in"))
+            {
+                string[] arr = fin.ReadLine().Split(' ');
+                int dimensiune = int.Parse(arr[0]);
+                int[] number = new int[dimensiune];
+                bool gasit;
+                for (int i = 0; i < dimensiune; i++)
+                {
+                    number[i] = int.Parse(arr[i + 1]);
+                }
+                Console.Write("k: ");
+                int k = int.Parse(Console.ReadLine());
+                if (k > dimensiune || k < 1)
+                {
+                    Console.WriteLine("Numar invalid: ");
+                }
+                else
+                {
+                    do
+                    {
+                        gasit = false;
+                        for (int i = 0; i < k - 1; i++)
+                        {
+                            if (number[i] < number[i + 1])
+                            {
+                                int aux = number[i];
+                                number[i] = number[i + 1];
+                                number[i + 1] = aux;
+                                gasit = true;
+                            }
+                        }
+                    } while (gasit);
+                }
+                for (int i = 0; i < dimensiune; i++)
+                {
+                    Console.WriteLine(number[i]);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
