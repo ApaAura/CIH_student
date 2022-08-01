81ca18d425b6f78daa8fd2ddeee98eb80859293f
Apareci Aura <aurica.apareci@gmail.com>
2022-08-01 23:33:11 +0300

    Laboratorul 4

---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 4/Problema 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_1
+{
+    /*
+     De creat o listă legată e conține informația despre calculatoare și volumul de memorie a
+     lui. Să se șteargă din această listă calculatoarele cu volumul de memorie minim. Să se afișeze lista
+     modificată.
+     */
+    struct Calculatoarele
+    {
+        public string Calculator;
+        public int Memorie;
+    }
+    class Program
+    {
+        static public void TiparesteLista(List<Calculatoarele> lista)
+        {
+            foreach (var el in lista)
+            {
+                Console.WriteLine($"{el.Calculator} {el.Memorie}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Calculatoarele> listaComp = new List<Calculatoarele>();
+            int nr = int.Parse(Console.ReadLine());
+            string denm;
+            int mr;
+            Console.WriteLine("Introduceti denumirea Calculatorului si volumul memoriei.");
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine($"Calculatorul {i}");
+                Console.Write("Introduceti deumirea calculatorului: ");
+                denm = Console.ReadLine();
+                Console.Write("Introduceti volumul de memorie (GB): ");
+                mr = int.Parse(Console.ReadLine());
+                listaComp.Add(new Calculatoarele() { Calculator = denm, Memorie = nr });
+            }
+            Console.WriteLine();
+            TiparesteLista(listaComp);
+            int min= listaComp[0].Memorie;
+            foreach (var el in listaComp)
+            {
+                if (el.Memorie > min) {min = el.Memorie; }
+            }
+            Console.WriteLine($"min {min}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 4/Problema 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problema_2
+{
+    public struct Carte
+    {
+        public string Denumire;
+        public int Pret;
+        public void Citire()
+        {
+            Console.Write("Denumire: ");
+            Denumire = Console.ReadLine();
+            Console.Write("Pret: ");
+            Pret = int.Parse(Console.ReadLine());
+        }
+        public void Afisare()
+        {
+            Console.WriteLine("===========================");
+            Console.WriteLine($"Denumirea cartii: {Denumire}");
+            Console.WriteLine($"Pret: {Pret}");
+            Console.WriteLine("===========================");
+        }
+    }
+    class Program
+    {
+        /*
+         De creat o coadă ce conține informație despre denumirea cărților și prețul lor. Să se afișeze
+         coada respectiva și prețul mediu al cărților din coadă.
+         */
+        static void Main(string[] args)
+        {
+            Queue<Carte> carte = new Queue<Carte>();
+            Console.Write("Introduceti numarul de carti: ");
+            int n = int.Parse(Console.ReadLine());
+            Carte nr1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Cartea {i+1}");
+                nr1 = new Carte();
+                nr1.Citire();
+                carte.Enqueue(nr1);
+            }
+            //Pretul mediu cu ajutorul System.Linq
+            float pretMediu = (float) carte.Average(x=>x.Pret);
+            Console.Clear();
+            Console.WriteLine("Elementele cozii: ");
+            foreach (var i in carte)
+            {
+                i.Afisare();
+            }
+            Console.WriteLine($"\nPretul mediu: {pretMediu}");
+        }
+    }
+}
