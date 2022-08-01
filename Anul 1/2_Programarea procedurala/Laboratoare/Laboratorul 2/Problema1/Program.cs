8e99e243c6a000cb880ea0e8af99d024d3f3d986
Apareci Aura <aurica.apareci@gmail.com>
2022-08-01 23:32:53 +0300

    Laboratorul 2

---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 2/Problema1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Problema1
+{
+    struct Elev
+    {
+        public string nume;
+        public double media;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elev[] listaelevi = new Elev[5];
+            Console.Write("Introdu lista elevilor:");
+            for (int i = 0; i < listaelevi.Length; i++)
+            {
+                Console.Write("Introdu numele: ");
+                listaelevi[i].nume = Console.ReadLine();
+                Console.Write("Introdu media: ");
+                listaelevi[i].media = double.Parse(Console.ReadLine());
+            }
+            using (BinaryWriter stream=new BinaryWriter(File.Open("Elevi.dat", FileMode.OpenOrCreate)))
+            {
+                foreach (Elev el in listaelevi)
+                {
+                    stream.Write(el.nume);
+                    stream.Write(el.media);
+                }
+                Console.WriteLine("Lista este scrisa in fisier!");
+            }
+            Elev[] lista = new Elev[5];
+            using (BinaryReader stream = new BinaryReader(File.Open("Elevi.dat",FileMode.Open)))
+            {
+                int i = 0;
+                Console.WriteLine("Lista elevilor: ");
+                while (stream.BaseStream.Position != stream.BaseStream.Length)
+                {
+                    lista[i].nume = stream.ReadString();
+                    lista[i].media = stream.ReadDouble();
+                    Console.WriteLine($"Numele: {lista[i].nume}, Media: {lista[i].media}");
+                    if (i==lista.Length-1)
+                    {
+                        break;
+                    }
+                    i++;
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Laboratoare/Laboratorul 2/Problema2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Problema2
+{
+    struct Elev
+    {
+        public string nume;
+        public double media;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           // Să se afișeze la ecran lista elevilor cu media mai mare ca 5 și lista elevilor restanțieri.
+            
+            Elev[] listaelevi = new Elev[5];
+            Console.Write("Introdu lista elevilor:");
+            for (int i = 0; i < listaelevi.Length; i++)
+            {
+                Console.Write("\nIntrodu numele: ");
+                listaelevi[i].nume = Console.ReadLine();
+                Console.Write("Introdu media: ");
+                listaelevi[i].media = double.Parse(Console.ReadLine());
+            }
+            using (BinaryWriter stream = new BinaryWriter(File.Open("Elevi.dat", FileMode.OpenOrCreate)))
+            {
+                foreach (Elev el in listaelevi)
+                {
+                    stream.Write(el.nume);
+                    stream.Write(el.media);
+                }
+            }
+            Elev[] lista = new Elev[5];
+            using (BinaryReader stream = new BinaryReader(File.Open("Elevi.dat", FileMode.Open)))
+            {
+                int i = 0;
+                while (stream.BaseStream.Position != stream.BaseStream.Length)
+                {
+                    lista[i].nume = stream.ReadString();
+                    lista[i].media = stream.ReadDouble();
+                    if (i == lista.Length - 1)
+                    {
+                        break;
+                    }
+                    i++;
+                }
+            }
+            Console.WriteLine("\nLista elevilor admisi");
+            for (int i = 0; i < listaelevi.Length; i++)
+            {
+                if (listaelevi[i].media>=5)
+                {
+                    Console.WriteLine($"{listaelevi[i].nume} {listaelevi[i].media}");
+                }
+            }
+            Console.WriteLine("Lista elevilor restantieri");
+            for (int i = 0; i < listaelevi.Length; i++)
+            {
+                if (listaelevi[i].media < 5)
+                {
+                    Console.WriteLine($"{listaelevi[i].nume} {listaelevi[i].media}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+        
+          
+
+
