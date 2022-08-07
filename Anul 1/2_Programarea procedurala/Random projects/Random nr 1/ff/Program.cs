5a8a7beb179da576e11bf4e4bcbb96607ed68b02
Apareci Aura <aurica.apareci@gmail.com>
2022-08-07 20:41:21 +0300

    Random projects 1

---
+++ Anul 1/2_Programarea procedurala/Random projects/Random nr 1/ff/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ff
+{
+    class Program
+    {
+        //static int n , m;
+        //static int[,] mas;
+
+
+        static int Main()
+        {
+            //CitireMasiv();
+            //CitieValori();
+            //AfisareMasiv();
+            //AfisareMax();
+            //AfisareMin();
+            //AfisareProd();
+
+            int i = 10;
+            int j = 10;
+            Console.WriteLine(i+" "+j);
+            show(i,out j);
+            Console.WriteLine(i + " " + j);
+            return 0;
+        }
+
+        private static void show(int i,out int j)
+        {
+            j = 15;
+            for (int z = 0; z < 10; z++)
+            {
+                i++;
+                j++;
+            }
+            Console.WriteLine(i + " " + j);
+        }
+
+        //private static void AfisareProd()
+        //{
+        //    Console.WriteLine($"Produsul este: {Produsul()}" );
+        //}
+
+        //private static int Produsul()
+        //{
+        //    int produs = 1;
+        //    foreach (int item in mas)
+        //    {
+        //        produs *= item;
+        //    }
+        //    return produs;
+        //}
+
+        //private static void AfisareMin()
+        //{
+        //    Console.WriteLine($"Elementul minim: {Min()}");
+        //}
+
+        //private static int Min()
+        //{
+        //    int min = int.MaxValue;
+        //    foreach (var item in mas)
+        //    {
+        //        min = (item < min) ? item:min;
+        //    }
+        //    return min;
+        //}
+
+        //private static void AfisareMax()
+        //{
+        //    Console.WriteLine($"Elementul maxim: {Max()}");
+        //}
+
+        //private static int Max()
+        //{
+        //    int max = int.MinValue;
+        //    foreach (var item in mas)
+        //    {
+        //        max = (item > max) ? item : max;
+        //    }
+        //    return max;
+        //}
+
+        //private static void AfisareMasiv()
+        //{
+        //    Console.Clear();
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < m; j++)
+        //        {
+        //            Console.Write(mas[i, j]+" "); 
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        //private static void CitieValori()
+        //{
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < m; j++)
+        //        {
+        //            mas[i, j] = int.Parse(Console.ReadLine());
+        //        }
+        //    }
+        //}
+
+        //private static void CitireMasiv()
+        //{
+        //    Console.Write("n = ");
+        //    n = int.Parse(Console.ReadLine());
+        //    Console.Write("m = ");
+        //    m = int.Parse(Console.ReadLine());
+        //    mas = new int[n, m];
+        //}
+    }
+}
---
+++ Anul 1/2_Programarea procedurala/Random projects/Random nr 1/vvv/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace vvv
+{
+    public static class Red
+    {
+        public async static void Logo()
+        {
+            string[] frames =
+            {
+                "_",
+                "_",
+                "_",
+                "-",
+                "`",
+                "`",
+                "'",
+                "´",
+                "-",
+                "_",
+                "_",
+                "_"
+            };
+            const string logo = "Oi Aurica, AAaAi Aurica, Oi Aurica, AAaAi Aurica";
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        foreach (var item in frames)
+                        {
+                            Thread.Sleep(80);
+                            Console.Title = item;
+                        }
+                    }
+                    Console.Title = "";
+                    foreach (var item in logo)
+                    {
+                        Console.Title += item;
+                        Thread.Sleep(100);
+                    }
+                    Thread.Sleep(920);
+                    for (int i = Console.Title.Length; i > 0; i--)
+                    {
+                        Thread.Sleep(100);
+                        Console.Title = Console.Title.Substring(0, i);
+
+                    }
+                }
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Red.Logo();
+            int x, y;
+            Citire(out x,out y);
+            AfisareF(x, y);
+        }
+
+        private static void Citire(out int x,out int y)
+        {
+            Console.WriteLine("Introduceti primul numar: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar: ");
+            y = int.Parse(Console.ReadLine());
+        }
+
+        private static void AfisareF(int x, int y)
+        {
+            Console.WriteLine($"F={Calcule(x,y)}");
+        }
+
+        private static double Calcule(int x, int y)
+        {
+            double F = (Math.Pow(x,y)+Math.Pow(y,x)) / (Math.Pow(x,-y)+Math.Pow(y,-x));
+                return  F;
+        }
+    }
+    
+}
