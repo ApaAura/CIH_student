90a89b0cbfd5cd0ea24c1322e39254b4285a0f05
Apareci Aura <aurica.apareci@gmail.com>
2022-08-07 20:41:34 +0300

    Random projects 2

---
+++ Anul 1/2_Programarea procedurala/Random projects/Random nr 2/ppp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            SwowSimplePercentage();
+            Console.ReadKey();
+        }
+        private static void SwowSimplePercentage()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"\rProgress: {i}%");
+                Thread.Sleep(25);
+            }
+            Console.Write("\rDone!               ");
+        }
+    }
+}
