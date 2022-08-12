40ddd94385bebe84fbf644eaee4679c7422f76d9
Apareci Aura <aurica.apareci@gmail.com>
2022-08-12 18:59:43 +0300

    Laboratorul 9

---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 9/Problema 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace Problema_1
+{
+    class Program
+    {
+        //Sa se fectueze plata unei sume S utilizand un numar minim de bancnote.
+        static void Main(string[] args)
+        {
+            int suma;
+            int[] nominal = new int[] { 1, 5, 10, 20, 50, 100, 200, 500, 1000 };
+            using (StreamReader rd=new StreamReader("date.in"))
+            {
+                 suma = int.Parse(rd.ReadLine());
+            }
+            MetodaGreedy(suma, nominal);
+            Console.ReadKey();
+            Console.WriteLine($"Suma este: {suma}");
+        }
+        private static void MetodaGreedy(int suma, int[] nominal)
+        {
+            int t = 0;
+            int i = nominal.Length - 1;
+            using (StreamWriter wr = new StreamWriter("date.out"))
+            {
+                wr.WriteLine($"Suma: {suma}");
+                Console.WriteLine($"Suma: {suma}");
+            }
+            while (suma>0)
+            {
+                if (suma>=nominal[i])
+                {
+                    Console.WriteLine($"{suma/nominal[i]} bancnote de valoarea {nominal[i]}");
+                }
+                t += suma / nominal[i];
+                suma %= nominal[i];
+                i--;
+            }
+            using (StreamWriter wr=new StreamWriter("date.out"))
+            {
+                wr.WriteLine($"Numarul de bancnote folosite {t}");
+                Console.WriteLine($"\nNumarul de bancnote folosite {t}");
+                Console.WriteLine("Datele au fost inscrise cu succes in fisier !");
+            }
+
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 9/Problema 2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Problema_2
+{
+    class Program
+    {
+         /*
+         Programul pentru minimizarea timpului de asteptare 
+         al unui client pentru a fi deservit la o statie.
+         */
+        static void Main(string[] args)
+        {
+            
+            using (StreamReader rd=new StreamReader("date.in"))
+            {
+                using (StreamWriter wr=new StreamWriter("date.out"))
+                {
+                    int nrClienti;
+                    nrClienti = int.Parse(rd.ReadLine());
+                    int[] TimpAsteptare = new int[nrClienti];
+                    for (int i = 0; i < nrClienti; i++)
+                    {
+                        TimpAsteptare[i] = int.Parse(rd.ReadLine());
+                    }
+                    List<int> timpClient = new List<int>();
+                    for (int i = 0; i < nrClienti; i++)
+                    {
+                        int Asteptare;
+                        Console.WriteLine($"Timpul de asteptare client {i}: {Asteptare = TimpAsteptare[i]}");
+                        timpClient.Add(Asteptare);
+                    }
+                    Ordonare(timpClient);
+                    wr.WriteLine($"Timpul total de asteptare are voaloarea minima: {CalculTimpMinim(timpClient)}");
+                    Console.WriteLine($"Timpul total de asteptare are voaloarea minima: {CalculTimpMinim(timpClient)}");
+                    Console.WriteLine("Datele au fost inscrise cu succes !");
+                }
+            }
+        }
+        private static object CalculTimpMinim(List<int> timpClient)
+        {
+            int s = 0;
+            Console.WriteLine("Ordinea de desrvire este: ");
+            for (int i = 1; i <= timpClient.Count; i++)
+            {
+                s += (timpClient.Count - 1 + 1) * timpClient[i - 1];
+                Console.Write($"{timpClient[i-1]} ");
+            }
+            Console.WriteLine();
+            return s;
+        }
+        private static void Ordonare(List<int> timpClient)
+        {
+            //Metoda bulelor
+            int k,aux;
+            do
+            {
+                k = 0;
+                for (int i = 0; i < timpClient.Count-1; i++)
+                {
+                    if (timpClient[1]>timpClient[i+1])
+                    {
+                        aux = timpClient[i];
+                        timpClient[i] = timpClient[i + 1];
+                        timpClient[i + 1] = aux;
+                        k = 1;
+                    }
+                }
+            } while (k==1);
+        }
+    }
+}
