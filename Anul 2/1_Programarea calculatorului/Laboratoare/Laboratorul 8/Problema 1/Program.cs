538d3137f379a062033fccd26454aa362920428c
Apareci Aura <aurica.apareci@gmail.com>
2022-08-12 18:59:32 +0300

    Laboratorul 8

---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 8/Problema 1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Problema_1
+{
+    /*
+     Se citeste un numar natural n. Afisati permutarile multimii 1,2,3...n in care
+     pana la jumatate elementele sunt in ordine descrescatoare, iar de la jumatate 
+     pana la final in ordine descrescatoare.
+     */
+    class Program
+    {
+        public static int nr;
+        public static int[] sol = new int[20];
+        public static int[] p = new int[20];
+
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                try
+                {
+                    Stopwatch stw = new Stopwatch();
+                    stw.Start();
+                    using (StreamWriter wr = new StreamWriter("date.out"))
+                    {
+                        wr.WriteLine("Permutarile obtinute: ");
+                    }
+                    Console.WriteLine("Pemutarile obtinute: ");
+                    Citire();
+                    Triere(1);
+                    long memorieProc = Process.GetCurrentProcess().WorkingSet64;
+                    stw.Stop();
+                    TimeSpan ts = stw.Elapsed;
+                    Console.WriteLine("Datele au fost inscrise cu succes !");
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                    Console.WriteLine($"\nTimpul de executie este: {elapsedTime}");
+                    Console.WriteLine($"Memoria alocata este: {memorieProc / 1000} Kbyte");
+                    isValid = true;
+                    Console.ReadKey();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Eroare !");
+                    isValid = false;
+                }
+            }
+        }
+        private static void Citire()
+        {
+            using (StreamReader rd=new StreamReader("date.in"))
+            {
+                nr = int.Parse(rd.ReadLine());
+            }
+        }
+        private static void Triere(int m)
+        {
+            for (int i = 0; i <= m; i++)
+            {
+                if (p[i]==0)
+                {
+                    sol[m] = i;
+                    p[i] = 1;
+                    if (Valid(m))
+                    {
+                        if (m==nr)
+                        {
+                            Afisare();
+                        }
+                        else
+                        {
+                            Triere(m + 1);
+                        }
+                        p[i] = 0;
+                    }
+                }
+            }
+        }
+        private static void Afisare()
+        {
+            using (StreamWriter wr=new StreamWriter("date.out", append:true))
+            {
+                for (int i = 1; i <= nr; i++)
+                {
+                    wr.Write($"{sol[i]} ");
+                    Console.Write($"{sol[i]}");
+                }
+                wr.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        private static bool Valid(int o)
+        {
+            if (o>1)
+            {
+                if (o<=(nr+1)/2)
+                {
+                    if (sol[o]<sol[o-1])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(sol[o]<sol[o-1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 8/Problema 2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Problema_2
+{
+    /*
+     Sa se afiseze in ordine alfabetica anagramele unui
+     cuvant format din literele distinte.
+     */
+    class Program
+    {
+        public static int nr;
+        public static char[] v = new char[100];
+        public static int[] st = new int[50];
+        public static int[] p = new int[50];
+        static void Main(string[] args)
+        {
+            Stopwatch stw = new Stopwatch();
+            stw.Start();
+            Citeste();
+            nr= v.Length;
+            Ordonez(v, nr);
+            Back(1);
+            long memorieProc = Process.GetCurrentProcess().WorkingSet64;
+            stw.Stop();
+            TimeSpan ts = stw.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine($"\nTimpul de executie este: {elapsedTime}");
+            Console.WriteLine($"Memoria alocata este: {memorieProc / 1000} Kbyte");
+        }
+        private static void Back(int k)
+        {
+            for (int i = 1; i <= nr; i++)
+            {
+                if (p[i] != 1)
+                {
+                    st[k] = i;
+                    p[i] = 1;
+                    if (k == nr) 
+                    {
+                        Scrie();
+                    }
+                    else
+                    {
+                        Back(k + 1);
+                    }
+                    p[i] = 0;
+                }
+            }
+        }
+        private static void Scrie()
+        {
+            using (StreamWriter wr=new StreamWriter("date.out"))
+            {
+                for (int i = 1; i <= nr; i++)
+                {
+                    wr.Write($"{v[st[i]-1]}");
+                    Console.Write($"{v[st[i] - 1]}");
+                }
+                wr.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        private static void Ordonez(char[] v, int n)
+        {
+            for (int i = 0; i < nr; i++)
+            {
+                for (int j = i+1; j < nr; j++)
+                {
+                    if (v[i]>v[j])
+                    {
+                        char aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+        }
+        private static void Citeste()
+        {
+            using (StreamReader rd=new StreamReader("date.in"))
+            {
+                v = rd.ReadLine().ToCharArray();
+            }
+        }
+    }
+}
