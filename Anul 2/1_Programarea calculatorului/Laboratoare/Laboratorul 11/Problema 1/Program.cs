968d2f4568e8603e0c33ca3ad856f461e6b58f5b
Apareci Aura <aurica.apareci@gmail.com>
2022-08-14 16:37:17 +0300

    Laboratorul 11

---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 11/Problema 1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace Problema_1
+{
+    class Program
+    {
+        //Determinarea elementului maxim prin tehnica Divide Et Impera
+        static void Main(string[] args)
+        {
+            using (StreamReader rd = new StreamReader("date.in"))
+            {
+                using (StreamWriter wr = new StreamWriter("date.out"))
+                {
+                    int nrElemente;
+                    nrElemente = int.Parse(rd.ReadLine());
+                    if (nrElemente==0)
+                    {
+                        Console.WriteLine("Nu au fost gasite elemente !");
+                    }
+                    else
+                    {
+                        int[] vector = new int[nrElemente];
+                        for (int i = 0; i < nrElemente; i++)
+                        {
+                            vector[i] = int.Parse(rd.ReadLine());
+                        }
+                        AfisareVector(vector);
+                        Console.WriteLine("\n\n*****************************");
+                        wr.WriteLine("*****************************");
+                        Console.WriteLine($"Elementul maxim este {MaxDivide(vector, 0, nrElemente-1)} ");
+                        wr.WriteLine($"Elementul maxim este {MaxDivide(vector, 0, nrElemente - 1)} ");
+                        Console.WriteLine("*****************************");
+                        wr.WriteLine("*****************************");
+                        Console.WriteLine("Elementele au fost inscrise cu succes !");
+                    }
+                }
+            }
+        }
+        private static int MaxDivide(int[] vector, int left, int right)
+        {
+            int els, eld, mijloc;
+            if (left==right)
+            {
+                return vector[left];
+            }
+            else
+            {
+                mijloc = (left+right) / 2;
+            }
+            els = MaxDivide(vector, left, mijloc);
+            eld = MaxDivide(vector, mijloc + 1, right);
+            if (els>eld)
+            {
+                return els;
+            }
+            else
+            {
+                return eld;
+            }
+        }
+        private static void AfisareVector(int[] vector)
+        {
+            Console.WriteLine("Elementele vectorului: ");
+            foreach (int i in vector)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Laboratoare/Laboratorul 11/Problema 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problema_2
+{
+    class Program
+    {
+        //Folosind metoda Devide Et Impera calculati suma elementelor unui vector
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul de elemente al vectorului: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            Random el = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = el.Next(0,100);
+            }
+            Console.WriteLine("Elementele masivului: ");
+            foreach (int i in vector)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine($"\nSuma elementelor vectorului: {SumaDivide(vector,0, n-1)}");
+        }
+        private static int SumaDivide(int[] vector, int left, int right)
+        {
+            if (left==right)
+            {
+                return vector[left];
+            }
+            else
+            {
+                return SumaDivide(vector, left, (left + right) / 2) + SumaDivide(vector, (left + right) / 2 + 1, right);
+            }
+        }
+    }
+}
