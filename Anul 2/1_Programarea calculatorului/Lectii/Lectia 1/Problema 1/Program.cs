c06053505d172a7c75faed02bc60f7f13434be11
Apareci Aura <aurica.apareci@gmail.com>
2022-08-16 12:47:57 +0300

    Lectia 1

---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 1/Problema 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddLast(1);
+            numbers.AddFirst(2);
+            numbers.AddAfter(numbers.Last, 3);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 1/Problema 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Problema_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(2.3);
+            list.Add(55);
+            list.AddRange(new string[] { "Apa", "Aura" });
+            foreach (object o in list)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine();
+            list.RemoveAt(0);
+            list.Reverse();
+            Console.WriteLine(list[0]);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
