bd28434b632dfd7c926fd911fc89ceb85444df7a
Apareci Aura <aurica.apareci@gmail.com>
2022-08-16 12:48:15 +0300

    Lectia 3

---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 3/Problema 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_1
+{
+    class Program
+    {
+        /*
+         Citeste de la tastatura un numar natural n si afiseaza numarul
+        bitilor din reprezentarea lui n in baza 2, apoi afiseaza bitii lui n.
+         */
+        static void Main(string[] args)
+        {
+            int n, num;
+            Stack<int> stiva = new Stack<int>();
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+            num = n;
+            while (n>0)
+            {
+                stiva.Push(n % 2);
+                n = n / 2;
+            }
+            Console.WriteLine($"Numarul {num} convertit in baza 2 are {stiva.Count} biti.");
+            Console.Write($"Numarul {num} convertit in baza 2 este : ");
+            while (stiva.Count>0)
+            {
+                Console.Write(stiva.Peek());
+                stiva.Pop();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 3/Problema 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_2
+{
+    class Program
+    {
+        //Sa se scrie functi ade inversare a unui sir folosind o stiva alocata dinamic.
+        static void Main(string[] args)
+        {
+            string sirul;
+            Console.Write("Introdu sirul: ");
+            sirul = Console.ReadLine();
+            Console.WriteLine($"Ati introdus sirul: {sirul}");
+            Console.WriteLine($"Sirul inversat este {InversareSir(sirul)}");
+            Console.ReadKey();
+        }
+        private static object InversareSir(string sir)
+        {
+            Stack<char> stiva = new Stack<char>();
+            string sirInversat = "";
+            for (int i = 0; i < sir.Length; i++)
+            {
+                stiva.Push(sir[i]);
+            }
+            while (stiva.Count>0)
+            {
+                sirInversat += stiva.Pop();
+            }
+            return sirInversat;
+        }
+    }
+}
