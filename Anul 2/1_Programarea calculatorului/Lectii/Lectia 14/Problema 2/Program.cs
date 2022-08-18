77736d0521bd2d609bc66ab0ffc627637b8c16a3
Apareci Aura <aurica.apareci@gmail.com>
2022-08-19 00:04:08 +0300

    Lectia 14

---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 14/Problema 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Problema_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //De elaborat o aplicație ce citește un șir de numere întregi din fișierul text cu numele NUMERE.IN. Completează o stivă cu aceste 
+            //numere și determină elementul maxim din această structură și va scrie rezultatul în fișierul NUMERE.OUT
+
+            StreamReader rd = new StreamReader("numere.in");
+            Stack<int> Stiva = new Stack<int>();
+            string[] ln = rd.ReadLine().Split(' ');
+            rd.Close();
+            int max = int.MinValue;
+            int nr;
+            foreach (var item in ln)
+            {
+                nr = int.Parse(item);
+                if (nr>max)
+                {
+                    max = nr;
+                }
+                Stiva.Push(nr);
+            }
+            Console.WriteLine("Elementele stivei: ");
+            foreach (int i in Stiva)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine($"\nElementul maximal: {max}");
+            using (StreamWriter wr = new StreamWriter("numere.out"))
+            {
+                wr.WriteLine(max);
+            }
+        }
+    }
+}
---
+++ Anul 2/1_Programarea calculatorului/Lectii/Lectia 14/Problema 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Problema_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //De elaborat o aplicație ce citește un șir de numere întregi din fișierul text cu numele NUMERE.IN. Completează o 
+            //coadă cu aceste numere și va adăuga valoarea 0 înaintea fiecărui element negativ și va scrie rezultatul în fișierul NUMERE.OUT.
+
+            StreamReader rd = new StreamReader("numere.in");
+            Queue<int> coada = new Queue<int>();
+            string[] ln = rd.ReadLine().Split(' ');
+            rd.Close();
+            int nr;
+            foreach (var item in ln)
+            {
+                nr = int.Parse(item);
+                coada.Enqueue(nr);
+            }
+            Console.WriteLine("Elementele cozii: ");
+            foreach (int i in coada)
+            {
+                Console.Write(i + " ");
+            }
+            Queue<int> coadaMod = new Queue<int>();
+            foreach (int i in coada)
+            {
+                if (i>=0)
+                {
+                    coadaMod.Enqueue(i);
+                }
+                else
+                {
+                    coadaMod.Enqueue(0);
+                    coadaMod.Enqueue(i);
+                }
+            }
+            Console.WriteLine("\nCoada modificata: ");
+            using (StreamWriter wr = new StreamWriter("numere.out"))
+            {
+                foreach (int i in coadaMod)
+                {
+                    Console.Write(i + " ");
+                    wr.Write(i + " ");
+                }
+            }
+           
+        }
+    }
+}
