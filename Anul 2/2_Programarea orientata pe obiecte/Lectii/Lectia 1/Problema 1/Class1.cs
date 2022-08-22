142e7f3f3e7f31a5533d3494fc67461a523a4ba0
Apareci Aura <aurica.apareci@gmail.com>
2022-08-22 21:21:52 +0300

    Lectia 1

---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 1/Problema 1/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problema_1
+{
+    class Cerculet
+    {
+        //campuri
+        private double raza;
+        private string culoare;
+        //proprietati
+        public double  Raza 
+        {
+            get { return raza; } 
+            set 
+            { 
+                if(value>0) 
+                    raza = value;
+                else
+                    raza = 3;
+            } 
+        }
+        public string Culoare
+        {
+            get { return culoare; }
+            set
+            {
+                if (value.Length!=0)
+                    culoare = value;
+                else
+                    culoare = "Rosu";
+            }
+        }
+        //Read data
+        public void Citire()
+        {
+            Console.Write("Introduceti raza cerculetului: ");
+            Raza = double.Parse(Console.ReadLine());
+            Console.Write("Introduceti culoarea cerculetului: ");
+            Culoare = Console.ReadLine();
+        }
+        //Display data
+        public void Afisare()
+        {
+            Console.WriteLine($"Raza cerculetului: {Raza}");
+            Console.WriteLine($"Culoarea cerculetului: {Culoare}");
+            Console.WriteLine($"Diametrul cerculetului: {CalculeazaDiametru():f3}");
+            Console.WriteLine($"Aria cerculetului: {CalculeazaAria():f3}");
+            Console.WriteLine($"Perimerul cerculetului: {CalculeazaPerimetrul():f3}");
+
+        }
+        //private methods
+        private double CalculeazaDiametru()
+        {
+            return raza * 2;
+        }
+        private double CalculeazaPerimetrul()
+        {
+            double perimetru = Math.Pow(raza, 2) * 2 * Math.PI;
+            return perimetru;
+        }
+        private double CalculeazaAria()
+        {
+            double aria = Math.PI * Math.Pow(raza, 2);
+            return aria;
+        }
+    }
+}
---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 1/Problema 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problema_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerculet Cerculet = new Cerculet();
+            Cerculet.Citire();
+            Console.Clear();
+            Cerculet.Afisare();
+        }
+    }
+}
