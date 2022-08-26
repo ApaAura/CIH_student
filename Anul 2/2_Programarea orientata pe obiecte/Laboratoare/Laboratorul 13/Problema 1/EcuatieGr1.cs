3545a496924ee9fb3941a6d32be94ab01cd2819a
Apareci Aura <aurica.apareci@gmail.com>
2022-08-26 09:37:25 +0300

    Laboratorul 13

---
+++ Anul 2/2_Programarea orientata pe obiecte/Laboratoare/Laboratorul 13/Problema 1/EcuatieGr1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1
+{
+    internal class EcuatieGr1
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public EcuatieGr1(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double X()
+        {
+            double x = 0;
+            x = (C - B) / A;
+            return x;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{A}x+{B}={C}");
+            Console.WriteLine($"X={X():F3}");
+        }
+    }
+}
---
+++ Anul 2/2_Programarea orientata pe obiecte/Laboratoare/Laboratorul 13/Problema 1/Program.cs
@@ -0,0 +1,5 @@
+﻿using Problema_1;
+EcuatieGr1 e1=new EcuatieGr1(12, 4, 6);
+EcuatieGr1 E2 = new EcuatieGr1(10, 4, e1.X());
+e1.Info();
+E2.Info();
