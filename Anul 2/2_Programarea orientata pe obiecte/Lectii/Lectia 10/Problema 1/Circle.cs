aba37c9a802f27a2b86e825333dadf423be868c4
Apareci Aura <aurica.apareci@gmail.com>
2022-08-31 00:03:03 +0300

    Lectia 10

---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 10/Problema 1/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1
+{
+    class Circle : Figure
+    {
+        double r;
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public override double Border()
+        {
+            return 2*r*Math.PI;
+        }
+        public override bool Exists()
+        {
+            bool val = false;
+            if (r>0)
+            {
+                val = true;
+            }
+            return val;
+        }
+        public override double Square()
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        public override string Str()
+        {
+            return $"Cerc: cu raza {r}";
+        }
+        public void Afisare()
+        {
+            if (Exists() == true)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine(Str());
+                Console.WriteLine($"Lungimea hotarului : {Border():f3} cm");
+                Console.WriteLine($"Aria : {Square():f3} cm^2");
+            }
+            else
+            {
+                Console.WriteLine("Nu se poate forma o figura !");
+            }
+        }
+    }
+}
---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 10/Problema 1/Program.cs
@@ -0,0 +1,5 @@
+﻿using Problema_1;
+Triangle T1=new Triangle(4,5,6);
+T1.Afisare();
+Circle C1=new Circle(8);
+C1.Afisare();
