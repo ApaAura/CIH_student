a9cee1bc98d0628c4015ce8e812698b185b0d43a
Apareci Aura <aurica.apareci@gmail.com>
2022-08-30 19:52:51 +0300

    Evaluare nr 1

---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectie de evaluare/Evaluare nr 1/Problema 1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Problema_1;
+SmartPhone SmartPhone1= new SmartPhone();
+SmartPhone1.Citire();
+SmartPhone1.Afisare();
+SmartPhone SmartPhone2= new SmartPhone( "Samsung","Galaxy A51",5000);
+SmartPhone2.Afisare();
+SmartPhone SmartPhone3 = new SmartPhone(SmartPhone1);
+SmartPhone3.Afisare();
---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectie de evaluare/Evaluare nr 1/Problema 1/SmartPhone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1
+{
+    internal class SmartPhone
+    {
+        public string Producator { get; set; }
+        public string Model { get; set; }
+        public int Pret { get; set; }
+        public SmartPhone()
+        {
+        }
+        public SmartPhone(string producator, string model, int pret)
+        {
+            Producator = producator;
+            Model = model;
+            Pret = pret;
+        }
+        public SmartPhone(SmartPhone smart)
+        {
+            Producator=smart.Producator;
+            Model=smart.Model;
+            Pret = smart.Pret;
+        }
+        public void Citire()
+        {
+            Console.WriteLine("Introduceti datele : ");
+            Console.Write("Producator : ");
+            Producator =Console.ReadLine();
+            Console.Write("Model : ");
+            Model = Console.ReadLine(); 
+            Console.Write("Pret : ");
+            Pret = int.Parse(Console.ReadLine());
+        }
+        //1 euro = 20.88 lei
+        public double PretEuro() =>Pret/20.88;
+        public void Afisare()
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Datele telefonului : ");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"Producator : {Producator}");
+            Console.WriteLine($"Model : {Model}");
+            Console.WriteLine($"Pretu in lei : {Pret}");
+            Console.WriteLine($"Pretul in euro :{PretEuro():F4} ");
+        }
+    }
+}
