e10b1eeba330d5e4cf3105151dec06df63d8de3b
Apareci Aura <aurica.apareci@gmail.com>
2022-09-02 08:06:36 +0300

    Lectia 13

---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 13/Problema 1/Numere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1
+{
+    internal class Numere
+    {   
+        public double Suma<T>(T numar1, T numar2)
+        {
+            return Convert.ToDouble(numar1)+Convert.ToDouble(numar2);
+        }
+        public double Produs<T>(T numar1, T numar2)
+        {
+            return Convert.ToDouble(numar1) * Convert.ToDouble(numar2);
+        }
+    }
+}
---
+++ Anul 2/2_Programarea orientata pe obiecte/Lectii/Lectia 13/Problema 1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Problema_1;
+Numere numere = new Numere();
+Console.WriteLine(numere.Suma(3,2.55));
+Console.WriteLine(numere.Suma("3","2.55"));
+Console.WriteLine(numere.Produs("3","6"));
+Console.WriteLine(numere.Produs(3,6));
+Console.WriteLine(numere.Produs(2.5,2));
