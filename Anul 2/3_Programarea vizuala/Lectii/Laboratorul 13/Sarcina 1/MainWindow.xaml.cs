bd9c34e6c7c8411edae235287fd5a1743ab80b91
Apareci Aura <aurica.apareci@gmail.com>
2022-09-08 00:08:58 +0300

    Laboratorul 13

---
+++ Anul 2/3_Programarea vizuala/Lectii/Laboratorul 13/Sarcina 1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sarcina_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button.Width += (Button.Width * 100) / 15;
+            Button.Height+=(Button.Height * 100)/ 15;
+            
+        }
+    }
+}
