3264d7bad5e4b7c8354600be6aca6f9a45e0fc42
Apareci Aura <aurica.apareci@gmail.com>
2022-09-08 00:08:41 +0300

    Laboratorul 11

---
+++ Anul 2/3_Programarea vizuala/Lectii/Laboratorul 11/Problema 2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problema_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBlock txtMesaj = new TextBlock();
+        Button cmdApasa = new Button();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ColumnDefinition coloana1 = new ColumnDefinition();
+            ColumnDefinition coloana2 = new ColumnDefinition();
+            aspectGrid.ColumnDefinitions.Add(coloana1);
+            aspectGrid.ColumnDefinitions.Add(coloana2);
+
+            cmdApasa.Content = "Apasa";
+            cmdApasa.Margin = new Thickness(20);
+            cmdApasa.FontSize = 34;
+            cmdApasa.Padding = new Thickness(20);
+            cmdApasa.HorizontalAlignment = HorizontalAlignment.Center;
+            cmdApasa.VerticalAlignment = VerticalAlignment.Center;
+            cmdApasa.Click += new RoutedEventHandler(cmdApasaClick);
+
+            Grid.SetColumn(cmdApasa, 0);
+            aspectGrid.Children.Add(cmdApasa);
+
+            txtMesaj.Margin = new Thickness(10);
+            txtMesaj.HorizontalAlignment = HorizontalAlignment.Center;
+            txtMesaj.VerticalAlignment = VerticalAlignment.Center;
+            txtMesaj.Foreground = Brushes.Teal;
+            txtMesaj.FontSize = 48;
+            Grid.SetColumn(txtMesaj, 1);
+            aspectGrid.Children.Add(txtMesaj);
+        }
+        private void cmdApasaClick(object sender, RoutedEventArgs e)
+        {
+            txtMesaj.Text = "Studiem WPF!";
+        }
+    }
+}
---
+++ Anul 2/3_Programarea vizuala/Lectii/Laboratorul 11/Sarcina 1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sarcina_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Count { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Count = 0;
+        }
+
+        private void Button(object sender, RoutedEventArgs e)
+        {
+            Count++;
+            label1.Content = $"{Count}";
+        }
+        private void Button1(object sender, RoutedEventArgs e)
+        {
+            Count--;
+            label1.Content = $"{Count}";
+        }
+    }
+}
