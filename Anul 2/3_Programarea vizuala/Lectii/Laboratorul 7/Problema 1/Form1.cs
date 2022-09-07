9ef9e37ae15fd8b5b44cab6915e7ee4a720d3a44
Apareci Aura <aurica.apareci@gmail.com>
2022-09-07 23:15:22 +0300

    Laboratorul 7

---
+++ Anul 2/3_Programarea vizuala/Lectii/Laboratorul 7/Problema 1/Form1.cs
@@ -0,0 +1,44 @@
+namespace Problema_1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox pictureBox1;
+        System.Windows.Forms.Timer timer1;
+        public Form1()
+        { 
+            InitializeComponent();
+            Default();
+        }
+        private void Default()
+        {
+            Size = new Size(400, 700);
+            Text = "Problema 1";
+            this.BackColor = Color.FromArgb(255, 247, 247, 247);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            pictureBox1 = new PictureBox();
+            pictureBox1.Image = new Bitmap("1.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Size = new Size(79, 140);
+            pictureBox1.Location = new Point(this.Width / 2 - pictureBox1.Size.Width / 2, this.Height - pictureBox1.Height);
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 100;
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+            this.Controls.Add(pictureBox1);
+        }
+
+        private void Timer1_Tick(object? sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 10);
+            if (pictureBox1.Location.Y < -1 * pictureBox1.Height)
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
