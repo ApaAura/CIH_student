e7f4f709b4fddeddac18fd709c75a59fd2898951
Apareci Aura <aurica.apareci@gmail.com>
2022-10-27 09:36:34 +0300

    Lesson task

---
+++ Anul 3/Other/Informatica/Lesson/Lectia7c.pas
@@ -0,0 +1,14 @@
+﻿
+var k,n:integer;
+function DIVI(k:integer; n:integer):integer;
+begin
+if(n mod k=0) then DIVI:=1
+else Divi:=0;
+end;
+
+begin
+write('n='); readln(n);
+write('k='); readln(k);
+if(DIVI(k,n)=1) then writeln(n+' este divizor al lui '+k)
+else writeln(n+' nu este divizor al lui '+k)
+end.
