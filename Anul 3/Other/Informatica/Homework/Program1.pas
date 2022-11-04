e29ed9ec700698e3bdf92c71cc9923c810e6ef5b
Apareci Aura <aurica.apareci@gmail.com>
2022-11-04 22:25:51 +0200

    Homework Tasks

---
+++ Anul 3/Other/Informatica/Homework/Program1.pas
@@ -0,0 +1,39 @@
+﻿var a,b:real;
+//varianta 1
+function Perimetrul(a:real;b:real):real;
+var P:real;
+begin
+P:=a+a+b+b;
+Perimetrul:=P;
+end;
+
+function Aria(a:real;b:real):real;
+var Ar:real;
+begin
+Ar:=a*b;
+Aria:=Ar;
+end;
+
+//varianta 2
+function Media(a:real;b:real):real;
+var Med:real;
+begin
+Med:=(a+b)/2;
+Media:=Med;
+end;
+
+function SumaPatratelor(a:real;b:real):real;
+var suma:real;
+begin
+suma:=a*a+b*b;
+SumaPatratelor:=suma;
+end;
+
+begin
+write('a = '); readln(a);
+write('b = '); readln(b);
+writeln('Periemtrul = '+Perimetrul(a,b));
+writeln('Aria = '+Aria(a,b));
+writeln('Media = '+Media(a,b));
+writeln('Suma patratelor = '+SumaPatratelor(a,b));
+end.
---
+++ Anul 3/Other/Informatica/Homework/Program2.pas
@@ -0,0 +1,22 @@
+﻿var n:real;
+//S(n)=1+3+5+...+(2*n-1)
+function Suma(n:real):real;
+begin
+if n=0 then Suma:=0
+else Suma:=(2*n-1)*Suma(n-1);
+end;
+
+//P(n)=1*4*7*...*(3*n-2)
+function Produs(n:real):real;
+var prod:real;
+begin
+if n=0 then prod:=0
+else prod:=(3*n-2)*Produs(n-1);
+Produs:=prod;
+end;
+
+begin
+write('n = '); readln(n);
+writeln('Suma recursiv = '+Suma(n));
+writeln('Produs recursiv = '+Produs(n));
+end.
