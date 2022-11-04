95bee48604098e6cd14367c8aa013426bbd59a2b
Apareci Aura <aurica.apareci@gmail.com>
2022-11-04 22:26:29 +0200

    Lesson task

---
+++ Anul 3/Other/Informatica/Lesson/Program1 VII.pas
@@ -0,0 +1,22 @@
+﻿var r:real;
+function Lungimea(r:real):real;
+var l:real;
+const pi=3.14;
+begin
+l:=2*pi*r;
+Lungimea:=l;
+end;
+
+function Aria(r:real):real;
+var a:real;
+const pi=3.14;
+begin
+a:=pi*r*r;
+Aria:=a;
+end;
+
+begin
+write('r = '); readln(r);
+writeln('Lungimea cercului '+Lungimea(r)+' cm');
+writeln('Aria cercului '+Aria(r)+' cm^2');
+end.
---
+++ Anul 3/Other/Informatica/Lesson/Program2 VII.pas
@@ -0,0 +1,17 @@
+﻿Var sir,inv:string;
+      
+function Inversare(sir:string):string;
+var  i:integer;
+begin
+inv:='';
+for i:=1 to Length(sir) do
+begin
+inv:=sir[i]+inv;
+end;
+Inversare:=sir;
+end;
+ 
+Begin
+writeln('Introduceti sirul de caractere: '); readln(sir);
+writeln('Sirul inversat este: '+Inversare(sir));
+End.
