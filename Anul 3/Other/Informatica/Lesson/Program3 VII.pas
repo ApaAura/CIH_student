b3ff38fc73cc75fd0365e3360399c0b15e5be9d7
Apareci Aura <aurica.apareci@gmail.com>
2022-11-10 22:12:07 +0200

    Lesson task

---
+++ Anul 3/Other/Informatica/Lesson/Program3 VII.pas
@@ -0,0 +1,16 @@
+﻿Var sir,inv:string;
+      
+function inversare(s:string):string;
+var n,i:integer;
+sir:string;
+begin
+sir:='';
+for i:=length(s) downto 1 do
+if s[i]<>' ' then sir:=sir+s[i];
+inversare:=sir;
+end;
+ 
+Begin
+writeln('Introduceti sirul de caractere: '); readln(sir);
+writeln('Sirul inversat este: '+inversare(sir));
+End.
