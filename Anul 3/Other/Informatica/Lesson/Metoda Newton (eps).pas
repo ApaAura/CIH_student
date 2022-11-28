ad9761b88750ea681ef53c3edc4a3555aba51e08
Apareci Aura <aurica.apareci@gmail.com>
2022-11-28 08:53:36 +0200

    Lesson task

---
+++ Anul 3/Other/Informatica/Lesson/Metoda Newton (eps).pas
@@ -0,0 +1,25 @@
+﻿program cn10;
+
+var
+  a, b, xn, xv, M2, m1, e, c: real;
+
+function f(z: real): real;
+begin f := cos(z) * cos(z) - z / 4; end;
+
+function fd1(z: real): real;
+begin fd1 := -sin(2 * z) - 1 / 4; end;
+
+begin
+  a := 2.4; b := 3; M2 := 2; m1 := 0.03; e := 0.0001;
+  c := a - (f(a)) / (f(b) - f(a)) * (b - a);
+  if f(c) * f(a) < 0 then begin
+    xn := a; xv := b;
+  end
+  else begin xn := b; xv := a; end;
+  while M2 * sqr(xn - xv) / (2 * m1) > e do
+  begin
+    xv := xn;
+    xn := xv - f(xv) / fd1(xv);
+    writeln(' x=', xn:15:12, ' f=', f(xn):15:12);
+  end;
+end.
---
+++ Anul 3/Other/Informatica/Lesson/Metoda Newton (n).pas
@@ -0,0 +1,23 @@
+﻿program cn09;
+
+var
+  a, b, x, c: real;
+  i, n: integer;
+
+function f(z: real): real;
+begin f := z * z * z - 2 * z * z + z - 3; end;
+
+function fd1(z: real): real;
+begin fd1 := 3 * z * z - 4 * z + 1; end;
+
+begin
+  a := 2.1; b := 15; n := 10; i := 0;
+  c := a - (f(a)) / (f(b) - f(a)) * (b - a);
+  if f(c) * f(a) < 0 then x := a else x := b;
+  while i < n do
+  begin
+    i := i + 1;
+    x := x - f(x) / fd1(x);
+    writeln('i=', i:2, ' x=', x:15:12, ' f=', f(x):15:12);
+  end;
+end.
