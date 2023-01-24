ee31c38b6b804b115d76ad5a83780283f78e7fa4
Apareci Aura <aurica.apareci@gmail.com>
2023-01-24 14:22:40 +0200

    Laboratorul 2

---
+++ Anul 3/6_APCS/Laboratoare/laborator_2/js/main.js
@@ -0,0 +1,24 @@
+/*Elaborați o aplicație ce va afișa în consola browserului produsul numerelor 25 și 65.
+console.log(`25*65 = ${25*65}`)*/
+
+/*Elaborați o aplicație ce va afișa media numerelor 34, 42, 12 în consola browserului.
+console.log(`Media dintre numerele 34,42 si 12 este ${((34+42+12)/3).toFixed(3)}`)*/
+
+/*Elaborați o aplicație ce va afișa în documentul web, consolă și o fereastră de alertare stringul ”Salut JavaScript”.
+console.log(`Salut JavaScript !`)
+let mesaj=alert(`Salut JavaScript !`)
+document.write(`Salut JavaScript !`) */
+
+/*Scrieți un program JavaScript care va calcula aria unui triunghi oarecare unde lungimile celor trei laturi ale acestuia sunt citite de la tastatura. Tratați excepțiile.
+let a=Number(prompt(`Introduceti latura 1 : `))
+let b=Number(prompt(`Introduceti latura 2 : `))
+let c=Number(prompt(`Introduceti latura 3 : `))
+
+if((a+b>c)&&(a+c>b)&&(b+c>a)){
+    let semiperimetru=((a+b+c)/2).toFixed(0)
+    let aria=confirm(`Aria triunghiului este ${(Math.sqrt(semiperimetru*(semiperimetru-a)*(semiperimetru-b)*(semiperimetru-c))).toFixed(3)} cm^2`)
+}
+else{
+    alert(`Nu se poate forma un triunghi !`)
+}*/
+
