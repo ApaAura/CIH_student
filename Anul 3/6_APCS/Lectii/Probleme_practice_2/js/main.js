d976469c5b8e37dbc4683941f86d33a8469cb97c
Apareci Aura <aurica.apareci@gmail.com>
2023-01-24 14:22:54 +0200

    Probleme practice nr 2

---
+++ Anul 3/6_APCS/Lectii/Probleme_practice_2/js/main.js
@@ -0,0 +1,53 @@
+/*Sarcină practică 1. Creați o variabilă numită "num" care să conțină numărul 1.*/
+let num=1
+/*Sarcină practică 2. Utilizând o buclă "for", iterați de la num la 10 și afișați fiecare număr pe ecran.
+for( num; num<10; num++){
+    console.log(num)
+}*/
+
+/*Sarcină practică 3. Utilizând o buclă "while", iterați de la num la 10 și afișați doar numerele pare.
+while(num<10){
+    if(num%2==0){
+        console.log(num)
+    }
+    num++
+}*/
+
+/*Sarcină practică 4. Utilizând o instrucțiune "if-else", verificați fiecare număr și afișați un mesaj diferit 
+pentru numerele mai mari sau mai mici decât 5.
+for( num; num<10; num++){
+    if(num==5){
+        console.log(`Numarul este 5 !`)
+        
+    }
+    else{
+        if(num<5){
+            console.log(`${num} este mai mic ca 5`)
+            }
+            else
+            {
+                console.log(`${num} este mai mare ca 5`)
+            }
+    }
+}*/
+
+/*Sarcină practică 5. Adăugați o instrucțiune "switch" care să verifice ultimul număr din interval și să
+afișeze un mesaj diferit în funcție de valoare (de exemplu, Ultimul număr este 9 ).
+Sarcină practică 6. Adăugați o opțiune în instrucțiunea "switch" care să afișeze un mesaj pentru
+cazul în care numărul nu se potrivește cu niciunul dintre cazurile definite.
+while(num){
+    switch(num){
+        case 1: console.log(`Numarul 1`); break;
+        case 2: console.log(`Numarul 2`); break;
+        case 3: console.log(`Numarul 3`); break;
+        case 4: console.log(`Numarul 4`); break
+        case 5: console.log(`Numarul 5`); break;
+        case 6: console.log(`Numarul 6`); break;
+        case 7: console.log(`Numarul 7`); break;
+        case 8: console.log(`Numarul 8`); break;
+        case 9: console.log(`Ultimul numar 9`); break;
+        default:
+            console.log(`Bucla while s-a finisat !`); break;
+    }
+    num++
+}*/
