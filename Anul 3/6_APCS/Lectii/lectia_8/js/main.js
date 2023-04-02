56d3b2f8dca5c444506ef1f96af1580811e25e68
Apareci Aura <aurica.apareci@gmail.com>
2023-04-02 20:31:53 +0300

    Lectia 8

---
+++ Anul 3/6_APCS/Lectii/lectia_8/js/main.js
@@ -0,0 +1,14 @@
+const showInfo=()=>{
+    alert(`Salut!`)
+}
+const showInfo2=()=>{
+    alert(`Heyoooo !`)
+}
+const showInfo3=()=>{
+    alert(`Vrei sa te salut ?`)
+}
+const schimbaCuloarea=()=>{
+    const btn=document.getElementById('btn')
+    btn.style.backgroundColor='red'
+    btn.style.color='green'
+}
